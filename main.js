48b143241a5e2093e79b5014d4ee0fb731e715bc
joaops2001 <69969148+joaops2001@users.noreply.github.com>
2024-02-20 22:08:36 -0300

    Added contact, clients and services
    Added contact, clients and services

---
+++ main.js
@@ -0,0 +1,41 @@
+/*ida menu*/
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/* volta menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+// mudar header ao rolar
+const header = document.querySelector('header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (this.window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+
+// swiper
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
