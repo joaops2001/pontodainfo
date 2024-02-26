c04c08ee116626ac54bd163ae7a84aea9245a4d3
joaops2001 <69969148+joaops2001@users.noreply.github.com>
2024-02-26 16:22:40 -0300

    ScrollReveal update

--- main.js
+++ main.js
@@ -43,3 +43,31 @@ const swiper = new Swiper('.swiper-container', {
     prevEl: '.swiper-button-prev'
   }
 })
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #clients header, #clients .clients
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
+
+//Back to top
+const backToTop = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 360) {
+    backToTop.classList.add('show')
+  } else {
+    backToTop.classList.remove('show')
+  }
+})
